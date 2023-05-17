54f6700c7d94f52fe37db3ca1cece3ebbe576b7b
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-05-17 22:15:47 +0530

    Context basic implementation

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ import Counter from './Questions/UseMemo/Counter';
 import Todo from './Questions/UseReducer/Todo';
 import Rough from './Questions/UseReducer/Rough';
 
-
+import MyContext from './Questions/UseContext/Context';
+import CounterContext from './Questions/UseContext/CounterContext';
+import Component1 from './Questions/UseContext/Component1'
 
 
 function App() {
+  const nanu = 'helonamu'
   return (
     <div className="App">
+
+
 
      {/* <Drop /> */}
      {/* <Image/> */}
  {/* <Todo/> */}
-<Counter />
+{/* <Counter /> */}
+
+
+<CounterContext />
+<MyContext.Provider value={'This is the data coming from App.jsx'}>
+  <Component1/>
+</MyContext.Provider>
  
     </div>
   );
